a4db2cc76f3f335f72dbc226cde2ca8ad92db1a7
norwegianblackmetal <finojenok.m@gmail.com>
2021-09-25 19:14:41 +0300

    changes

--- main.tf
+++ main.tf
@@ -1,26 +1,25 @@
 terraform {
   required_providers {
     virtualbox = {
-      source = "terra-farm/virtualbox"
+      source  = "terra-farm/virtualbox"
       version = "0.2.2-alpha.1"
     }
   }
 }
 
 resource "virtualbox_vm" "node" {
-    count = 2
-    name = "${format("node-%02d", count.index+1)}"
-    url = "https://vagrantcloud.com/ubuntu/boxes/xenial64/versions/20180420.0.0/providers/virtualbox.box"
-    image = "./virtualbox.box"
-    cpus = 2
-    memory = "512 mib"
-   // user_data = "${file("user_data")}"
+  count  = 1
+  name   = format("node-%02d", count.index + 1)
+  image  = "./CentOS-7-x86_64-DVD-2009.iso"
+  cpus   = 2
+  memory = "2048 mib"
+  // user_data = "${file("user_data")}"
 
 
-     network_adapter {
-       type = "bridged"
-       host_interface="en0"
-    }
+  network_adapter {
+    type           = "bridged"
+    host_interface = "en0"
+  }
 
 
 }
